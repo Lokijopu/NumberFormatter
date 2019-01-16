a4fe3afc07e5359680293dc6599902dc1dc4e7a9
Lokijopu <kguo2134@gmail.com>
2019-01-16 00:29:42 -0500

    Update DefaultFormatter.java

--- src/DefaultFormatter.java
+++ src/DefaultFormatter.java
@@ -7,15 +7,15 @@ public class DefaultFormatter implements NumberFormatter{
 	public String toString(int num) {
 		String str = Integer.toString(num);
 		String temp = "";
-		for (int i = 0; i < str.length(); i++) {
-			if (i % 3 == 0  && i > 0) {
+		for (int i = str.length() - 1; i >= 0; i--) {
+			if ((str.length() - i) % 3 == 0 && (str.length() - i) < str.length()) {
 				temp += str.substring(i, i+1) + ",";
 			} else {
 				temp += str.substring(i, i+1);
 			}
 		}
 		String finalStr = "";
-		for (int i = temp.length(); i > 0; i++) {
+		for (int i = temp.length() - 1; i >= 0; i--) {
 			finalStr += temp.substring(i, i+1);
 		}
 		return finalStr;
