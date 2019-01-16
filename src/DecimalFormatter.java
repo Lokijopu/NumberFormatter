f6253268a3e3a1908aa4dbf02ed164a96e9064cb
Lokijopu <kguo2134@gmail.com>
2019-01-16 00:30:10 -0500

    Update DecimalFormatter.java

--- src/DecimalFormatter.java
+++ src/DecimalFormatter.java
@@ -5,21 +5,19 @@
  */
 public class DecimalFormatter implements NumberFormatter{
 	public String toString(int num) {
-		public String toString(int num) {
-			String str = Integer.toString(num);
-			String temp = "";
-			for (int i = 0; i < str.length(); i++) {
-				if (i % 3 == 0  && i > 0) {
-					temp += str.substring(i, i+1) + ".";
-				} else {
-					temp += str.substring(i, i+1);
-				}
+		String str = Integer.toString(num);
+		String temp = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			if ((str.length() - i) % 3 == 0 && (str.length() - i) < str.length()) {
+				temp += str.substring(i, i+1) + ".";
+			} else {
+				temp += str.substring(i, i+1);
 			}
-			String finalStr = "";
-			for (int i = temp.length(); i > 0; i++) {
-				finalStr += temp.substring(i, i+1);
-			}
-			return finalStr;
 		}
+		String finalStr = "";
+		for (int i = temp.length() - 1; i >= 0; i--) {
+			finalStr += temp.substring(i, i+1);
+		}
+		return finalStr;
 	}
 }
